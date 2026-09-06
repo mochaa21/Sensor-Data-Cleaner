c95ec59d5b4f625987ba4b44e054d31ba9ccb4d1
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-06 11:21:32 +0700

    fix: refactoring

--- sdc.py
+++ sdc.py
@@ -1,9 +1,7 @@
-
 raw_data = " 100, 25, 10, 50, 25, 100, 50, 75, 10 "
 # Expectation: [10, 25, 50, 75, 100]
 data = []
 raw_data_list = raw_data.strip().split(', ')
-print(raw_data_list)
 for raw in raw_data_list:
     data_int = int(raw)
     data.append(data_int)
